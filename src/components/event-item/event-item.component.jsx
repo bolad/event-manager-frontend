7ac43f83090e8aedb6bb390010858f9dc9422f46
stanley akyea <stanley.akyea@gmail.com>
2020-09-24 12:05:03 +0200

    Add imageUrl links to event items

--- src/components/event-item/event-item.component.jsx
+++ src/components/event-item/event-item.component.jsx
@@ -2,8 +2,14 @@ import React from 'react';
 
 import './event-item.styles.scss';
 
-const EventItem = ({ title }) => (
+const EventItem = ({ title ,imageUrl }) => (
   <div className="event-item">
+    <div
+      className='background-image'
+      style={{
+        backgroundImage: `url(${imageUrl})`
+      }}
+    />
     <div className="content">
     <h1 className="title">{ title }</h1>
     <span className="subtitle">CHECK OUT EVENTS</span>
--- src/components/events-directory/events-directory.component.jsx
+++ src/components/events-directory/events-directory.component.jsx
@@ -1,7 +1,6 @@
 import React from 'react';
 
 import EventItem from '../event-item/event-item.component';
-
 import './events-directory.styles.scss';
 
 class EventsDirectory extends React.Component {
@@ -12,23 +11,27 @@ class EventsDirectory extends React.Component {
       sections: [
         {
           title: 'Music',
+          imageUrl: 'https://i.ibb.co/17Ybhzy/music2.jpg',
           id: 1,
-
         },
         {
           title: 'Sports',
+          imageUrl: 'https://i.ibb.co/9Wx6FWD/sports.jpg',
           id: 2,
         },
         {
           title: 'Education',
+          imageUrl: 'https://i.ibb.co/KqrJS40/doug-linstedt-j-EEYZsaxb-H4-unsplash.jpg',
           id: 1,
         },
         {
           title: 'Arts',
+          imageUrl: 'https://i.ibb.co/yq3Qr2D/mike-von-Osjura-Wm-Xm-U-unsplash.jpg',
           id: 1,
         },
         {
           title: 'Technology',
+          imageUrl: 'https://i.ibb.co/D9L07MX/tech.jpg',
           id: 1,
         }
       ]
