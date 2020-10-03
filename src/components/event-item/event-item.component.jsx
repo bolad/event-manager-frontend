f8d6355b3a134cfda6aaadc8113611567fcc3919
stanley akyea <stanley.akyea@gmail.com>
2020-10-04 01:37:28 +0200

    Modify event item styles

--- src/components/event-item/event-item.component.jsx
+++ src/components/event-item/event-item.component.jsx
@@ -2,26 +2,18 @@ import React from 'react';
 
 import './event-item.styles.scss';
 
-const EventItem = ({ name, price, alloted_tickets, start_date, start_time, imageUrl }) => {
-
-  return (
-    <div className="event-item">
-      <div
-        className='background-image'
-        style={{
-          backgroundImage: `url(${imageUrl})`
-        }}
-      />
-      <div className="event-footer">
-        <h2 className="name">{name}</h2>
-        <span className="start-date">Date: {start_date}</span>
-        <span className="start-time">Time: {start_time}</span>
-        <span className="price">${price}</span>
-        <span className="alloted-tickets">Tickets Available: {alloted_tickets}</span>
-      </div>
+const EventItem = ({ name, imageUrl }) => (
+  <div className="event-item">
+    <div
+      className='background-image'
+      style={{
+        backgroundImage: `url(${imageUrl})`,
+      }}
+    />
+    <div className="event-details">
+      <span className="name">{name}</span>
     </div>
-  )
-  
-};
+  </div>
+);
 
 export default EventItem;
