4b4559d8f757f0c262f9bdab125e604a65e47cae
stanley akyea <stanley.akyea@gmail.com>
2020-10-13 23:03:45 +0200

    Configure tailwindcss to render event items

--- src/components/event-item/event-item.component.jsx
+++ src/components/event-item/event-item.component.jsx
@@ -1,19 +1,33 @@
 import React from 'react';
 
-import './event-item.styles.scss';
+//import './event-item.styles.scss';
 
-const EventItem = ({ name, imageUrl }) => (
-  <div className="event-item">
-    <div
-      className='background-image'
-      style={{
-        backgroundImage: `url(${imageUrl})`,
-      }}
-    />
-    <div className="event-details">
-      <span className="name">{name}</span>
+const EventItem = ({ name, imageUrl, start_date, start_time, price }) => {
+  return (
+    <div className="max-w-sm rounded overflow-hidden shadow-lg">
+      <img src={imageUrl} alt="" className="w-full"/>
+      <div className="px-6 py-4">
+        <div className="font-bold text-purple-500 text-xl mb-2">
+          {name}
+        </div>
+        <ul>
+          <li>
+            <strong>Date: </strong>
+            {start_date}
+          </li>
+          <li>
+            <strong>Time: </strong>
+            {start_time}
+          </li>
+          <li>
+            <strong>Price: </strong>
+            ${price}
+          </li>
+        </ul>
+      </div>
+     
     </div>
-  </div>
-);
+  )
+}
 
 export default EventItem;
--- src/pages/events/events-page.component.jsx
+++ src/pages/events/events-page.component.jsx
@@ -16,18 +16,21 @@ const EventsPage = () => {
   }, [])
 
   return (
-    <div events-preview>
-      {events.map(event =>
-        <EventItem 
-          key={event.id}
-          name={event.name}
-          start_date={event.start_date}
-          start_time={event.start_time}
-          price={event.price}
-          alloted_tickets={event.alloted_tickets}
-          imageUrl={event.image.url}
-        />
-      )}
+    <div className="container mx-auto">
+      <div className="grid grid-cols-3 gap-4">
+        {events.map(event =>
+          <EventItem 
+            key={event.id}
+            name={event.name}
+            start_date={event.start_date}
+            start_time={event.start_time}
+            price={event.price}
+            alloted_tickets={event.alloted_tickets}
+            imageUrl={event.image.url}
+          />
+        )}
+      </div>
+      
     </div>
   )
 
