03f9d9879ff7e298c400c88a060e0c8814a828d0
stanley akyea <stanley.akyea@gmail.com>
2020-11-01 12:32:35 +0100

    Change event item display styles

--- src/components/event-item/event-item.component.jsx
+++ src/components/event-item/event-item.component.jsx
@@ -2,29 +2,16 @@ import React from 'react';
 
 import './event-item.styles.scss';
 
-const EventItem = ({ name, imageUrl, start_date, start_time, price }) => {
+const EventItem = ({ name, imageUrl, price }) => {
   return (
     
       <div className="max-w-sm rounded overflow-hidden shadow-lg card-container">
         <img src={imageUrl} alt="" className="w-full"/>
         <div className="px-6 py-4">
-          <div className="font-bold text-purple-500 text-xl mb-2">
-            {name}
+          <div className="event-footer">
+             <span className="name">{name}</span>
+             <span className="price">${price}</span>
           </div>
-          <ul>
-            <li>
-              <strong>Date: </strong>
-              {start_date}
-            </li>
-            <li>
-              <strong>Time: </strong>
-              {start_time}
-            </li>
-            <li>
-              <strong>Price: </strong>
-              ${price}
-            </li>
-          </ul>
         </div>
       
       </div>
