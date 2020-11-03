f57fc78de333b121bc847a93e70ef85ffa45808c
stanley akyea <stanley.akyea@gmail.com>
2020-11-03 21:20:02 +0100

    Change event item display

--- src/components/event-item/event-item.component.jsx
+++ src/components/event-item/event-item.component.jsx
@@ -4,13 +4,13 @@ import './event-item.styles.scss';
 
 const EventItem = ({ name, imageUrl, price }) => {
   return (
-    <div className="max-w-sm rounded overflow-hidden shadow-lg card-container">
-      <img src={imageUrl} alt="" className="w-full"/>
-      <div className="px-6 py-4">
-        <div className="event-footer">
-            <span className="name">{name}</span>
-            <span className="price">${price}</span>
-        </div>
+    <div className="flex h-full bg-white rounded overflow-hidden shadow-lg">
+      <div className="w-full md:w-1/3 rounded-t">
+        <img src={imageUrl} alt="" className="w-full event-img" />
+      </div>
+      <div className="w-full md:w-2/3 flex flex-col flex-grow flex-shrink">
+        <span className="name">{name}</span>
+        <span className="price">${price}</span>
       </div>
     </div> 
   )
--- src/components/event-list/event-list.component.jsx
+++ src/components/event-list/event-list.component.jsx
@@ -1,6 +1,5 @@
 import React from 'react';
 import EventItem from '../event-item/event-item.component';
-import { Link } from 'react-router-dom';
 
 import './event-list.styles.scss';
 
@@ -8,14 +7,12 @@ export const EventList = ({events}) => {
     return (
     <div className="event-list">
       {events.map(event =>
-      <Link to={`/events/${event.id}`}>
         <EventItem 
           key={event.id}
           name={event.name}
           price={event.price}
           imageUrl={event.image.url}
         />
-        </Link>
       )}
     </div>
     )
