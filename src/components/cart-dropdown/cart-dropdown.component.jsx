d6dede5f498d5b36968f7bb9fae9ee2fb2c9be2b
stanley akyea <stanley.akyea@gmail.com>
2020-11-04 11:02:45 +0100

    Add cart items to cart dropdown component

--- src/components/cart-dropdown/cart-dropdown.component.jsx
+++ src/components/cart-dropdown/cart-dropdown.component.jsx
@@ -1,15 +1,28 @@
 import React, { useContext } from 'react';
 import { CartContext } from '../../providers/cart/cart.provider';
 import CustomButton from '../custom-button/custom-button.component';
+import CartItem from '../cart-item/cart-item.component';
 
 import './cart-dropdown.styles.scss';
 
 const CartDropdown = () => {
   const { cartItems } = useContext(CartContext);
-  
+
   return (
     <div className='cart-dropdown'>
-      <div className='cart-items' />
+       <div className='cart-items'>
+        {cartItems.length ? (
+          cartItems.map(cartItem => (
+            <CartItem 
+              event={cartItem}
+              key={cartItem.id} 
+              imageUrl={cartItem.image.url}
+            />
+          ))
+        ) : (
+          <span className='empty-message'>Your cart is empty</span>
+        )}
+      </div>
       <CustomButton>GO TO CHECKOUT</CustomButton>
     </div>
   );
