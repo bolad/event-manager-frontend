7b2a15ac1f2da91e5296fd789c6c9bdaf6f99d7b
stanley akyea <stanley.akyea@gmail.com>
2020-11-04 13:19:13 +0100

    Create checkout item component and display in checkout page

--- src/pages/checkout/checkout.component.jsx
+++ src/pages/checkout/checkout.component.jsx
@@ -1,5 +1,6 @@
 import React, { useContext } from 'react';
 import { CartContext } from '../../providers/cart/cart.provider';
+import CheckoutItem from '../../components/checkout-item/checkout-item.component';
 
 import './checkout.styles.scss';
 
@@ -8,7 +9,31 @@ const CheckoutPage = () => {
 
   return (
     <div className='checkout-page'>
-      Checkout Page
+       <div className='checkout-header'>
+        <div className='header-block'>
+          <span>Event</span>
+        </div>
+        <div className='header-block'>
+          <span>Description</span>
+        </div>
+        <div className='header-block'>
+          <span>Quantity</span>
+        </div>
+        <div className='header-block'>
+          <span>Price</span>
+        </div>
+        <div className='header-block'>
+          <span>Remove</span>
+        </div>
+      </div>
+      {cartItems.map(cartItem => (
+        <CheckoutItem 
+          key={cartItem.id} 
+          cartItem={cartItem}
+          imageUrl={cartItem.image.url} 
+        />
+      ))}
+      <div className='total'>TOTAL: ${cartTotal}</div>
     </div>
   );
 };
